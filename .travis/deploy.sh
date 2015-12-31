00c2ef05bf064c60554a746546e0a6a8843f58c3
Zaicheng Qi <vmlinz@gmail.com>
2015-12-31 20:35:59 +0800

    Add log for deploy.sh and toggle off gitcafe deploy for the moment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Decrypt the private key
+echo "Decrypt key"
 openssl aes-256-cbc -K $encrypted_3fec25997f5e_key -iv $encrypted_3fec25997f5e_iv -in .travis/ssh_key.enc -out ~/.ssh/id_rsa -d
 # Set the permission of the key
+echo "chmod for the key"
 chmod 600 ~/.ssh/id_rsa
 # Start SSH agent
 eval $(ssh-agent)
@@ -13,6 +15,8 @@ cp .travis/ssh_config ~/.ssh/config
 git config --global user.name "Blog Bot"
 git config --global user.email vmlinz+blogbot@gmail.com
 # Clone the repository
+echo "clone original blog site"
 git clone git@github.com:vmlinz/vmlinz.github.io.git .deploy_git
 # Deploy to GitHub
+echo "deploy new changes to blog site"
 npm run deploy
